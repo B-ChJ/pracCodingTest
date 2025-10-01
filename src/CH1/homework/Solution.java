bd5a4f501950a2d1ba90c95be3edd80cde5cf953
B-ChJ <bchaejoo@gmail.com>
2025-10-01 15:00:19 +0900

    Solution Class ✅ 정답 배열 answers 입력 과정 구현

--- src/CH1/homework/Solution.java
+++ src/CH1/homework/Solution.java
@@ -3,6 +3,9 @@ package CH1.homework;
 import java.util.Scanner;
 
 /**음악 테스트 응시자의 패턴
+ * 시험 문제의 정답이순서대로 들어있는 배열 answer가 주어졌을 때,
+ * 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return하는 함수를 작성하기
+
  * 응시자 3명
  * (1) 1번 응시자의 패턴: [도, 레, 미, 파]
       * 4개의 음계를 순차적으로 반복
@@ -23,9 +26,35 @@ import java.util.Scanner;
  * 출력
  * 가장 많은 문제를 맞힌 사람*/
 public class Solution {
-    Scanner input = new Scanner(System.in);
+    public int[] solution(String[] answers) {
+
+        int[] answer = {};
+        return answer;
+    }
 
     public static void main(String[] args) {
-        System.out.println("문제의 개수를 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        Solution solution = new Solution();
+
+        // 문제 개수 입력
+        int N = sc.nextInt();
+
+        // 정답 배열 입력
+        String[] answers = new String[N];
+        for (int i = 0; i < N; i++) {
+            answers[i] = sc.next();
+        }
+        for(int j = 0; j < N; j++) {
+        System.out.println(answers[j]);}
+        // 결과 출력
+//        int[] result = solution.solution(answers);
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < result.length; i++) {
+//            sb.append(result[i]);
+//            if (i < result.length - 1) {
+//                sb.append(" ");
+//            }
+//        }
+//        System.out.println(sb.toString());
     }
 }
